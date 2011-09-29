6b40936bc83391fbb77a185efe3131b3099c3565
± ryan <ryan@2sidedfigure.net>
2011-09-29 11:49:32 -0700

    Allow creation of keys for empty fields

--- jquery.form2json.js
+++ jquery.form2json.js
@@ -1,12 +1,22 @@
 (function($) {
     
     var defaults = {
-            inputSelectors: 'input:not([type=radio], [type=checkbox], [type=reset]), input[type=checkbox]:checked, input[type=radio]:checked, textarea, select',
+            inputSelectors: 'input:not([type=radio], [type=checkbox], [type=reset]), input[type=checkbox], input[type=radio]:checked, textarea, select',
             multiValSelector: '[type=checkbox], select',
             keyAttr: 'name',
-            wrapped: false
+            wrapped: false,
+            allowEmptyMultiVal: false,
+            allowEmptySingleVal: true
         },
-        settings = {};
+        settings = {},
+        // borrow isEmpty from underscore to prevent a hard dependency
+        // http://documentcloud.github.com/underscore
+        empty = function(obj) {
+            //if (_.isArray(obj) || _.isString(obj)) return obj.length === 0;
+            if (obj && obj.length) return obj.length === 0;
+            for (var key in obj) if (hasOwnProperty.call(obj, key)) return false;
+            return true;
+        };
     
     $.fn.form2json = function(options) {
         var form = $(this);
@@ -24,24 +34,29 @@
             
         singleVal.each(function() {
             var item = $(this),
-                key = item.attr(settings.keyAttr) || item.attr('name') || item.attr('id');
+                key = item.attr(settings.keyAttr) || item.attr('name') || item.attr('id'),
+                val = item.val();
             
-            key && (data[key] = item.val());
+            if (!settings.allowEmptySingleVal && empty(val)) return true;
+            (key) && (data[key] = val);
         });
         
         multiVal.each(function() {
             var item = $(this),
-                key = item.attr(settings.keyAttr) || item.attr('name') || item.attr('id');
+                key = item.attr(settings.keyAttr) || item.attr('name') || item.attr('id'),
+                val = item.is(':checkbox:not(:checked)') ? null : item.val();
             
-            if (key) {
+            if (key && (val || settings.allowEmptyMultiVal)) {
                 if (data[key]) {
-                    //already exists, but needs to be turned into an array
-                    $.isArray(data[key]) || (data[key] = [ data[key] ]);
-                    
-                    data[key].push(item.val());
+                    if (val) {
+                        //already exists, but needs to be turned into an array
+                        $.isArray(data[key]) || (data[key] = [ data[key] ]);
+                        
+                        data[key].push(val);
+                    }
                 } else {
                     //data doesn't have the item yet, create it
-                    data[key] = item.val();
+                    data[key] = val;
                 }
             }
         });
